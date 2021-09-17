0276594c225b0591f2d4fd6b8a80e4bf9e939ea1
xFGhoul <dauntejules9@gmail.com>
2021-09-17 13:36:43 -0700

    Base.

---
+++ auto.py
@@ -0,0 +1,95 @@
+import time
+import urllib.request 
+import json
+import shutil
+
+import os
+
+import win32com.client
+
+
+import inquirer
+from rich.console import Console
+
+
+start = time.time()
+
+console = Console()
+
+console.print("""[cyan1]
+ █████╗ ██╗   ██╗████████╗ ██████╗     ███████╗████████╗ █████╗ ██████╗ ████████╗
+██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗    ██╔════╝╚══██╔══╝██╔══██╗██╔══██╗╚══██╔══╝
+███████║██║   ██║   ██║   ██║   ██║    ███████╗   ██║   ███████║██████╔╝   ██║   
+██╔══██║██║   ██║   ██║   ██║   ██║    ╚════██║   ██║   ██╔══██║██╔══██╗   ██║   
+██║  ██║╚██████╔╝   ██║   ╚██████╔╝    ███████║   ██║   ██║  ██║██║  ██║   ██║   
+╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝     ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   
+                      Developer: Ghoul#6066                                                        
+[cyan1]""")
+
+console.print("[cyan1]I will now ask you a few questions so we can get started.\n")
+
+console.print("[cyan1]Before We Get Started I am Going To Check For Updates.")
+
+THIS_VERSION = "1.0.5"
+
+with urllib.request.urlopen("https://api.github.com/repos/:author/:repo/releases/latest") as url:
+  data = json.loads(url.read().decode())
+  latest = data['tag_name'][1:] # "v2.3.6" -> "2.3.6"
+  patchNotes = data['body']
+
+console.print("[cyan1]FOR THE CHOICES, AN EXAMPLE WOULD BE:\n\nDrive: [red]C[/red]\nProgram Name: [red]discord(MUST MATCH EXACTLY)[/red][/cyan1]\n\n")
+
+drive = console.input("[cyan1]Drive: [/cyan1]", markup=True)
+
+program_name = console.input("[cyan1]Program Name: [/cyan1]", markup=True)
+
+questions = [
+  inquirer.List('folder',
+                    message="Where Should I Search?",
+                    choices=['Program Files', 'Program Files (x86)', 'AppData'],
+                    ),
+]
+answer = inquirer.prompt(questions)
+
+console.print(f"\n\n[cyan1]Good Job Answering!, I will now start to search for [red]{program_name}.exe[/red] in Drive [red]{drive.upper()}:/[/red]\n\n")
+
+console.print(f"[red]NOTE: THIS PROCESS WILL SEARCH YOUR ENTIRE DRIVE(It Will Exlude Some Directories And File Types.), IT WILL NOT FINISH 'FAST', YOU WILL NEED TO BE PATIENT[/red]")
+
+username = os.getlogin()
+
+exclude = set(['Windows', 'Microsoft', 'Intel', 'Google', 'dotnet', 'ImageGlass', 'Inno Setup 6', 'Microsoft Visual Studio', 'Internet Explorer', 'Microsoft Visual Studio Tools for Unity', 'Microsoft SQL Server', 'Windows Portable Devices', 'Windows Media Player', 'Windows Multimedia Platform', 'Common Files', 'Microsoft Analysis Services', 'Microsoft Office', 'Microsoft SDKs', 'MSBuild', 'Windows NT', 'Windows Kits', 'Windows Photo Viewer', 'Windows Defender', 'WinRar', 'Windows Defender Advanced Threat Protection', 'Windows Security', 'WindowsApps', 'Microsoft.NET', 'K-Lite Codec Pack', 'Reference Assemblies'])
+
+for root, dirs, files in os.walk(f"{drive.upper()}:/Users/{username}/{answer['folder']}" if answer["folder"] == "AppData" else f"{drive.upper()}:/{answer['folder']}" if answer["folder"] == 'Program Files' or 'Program Files (x86)' else None, onerror=None, topdown=True):
+
+    [dirs.remove(d) for d in list(dirs) if d in exclude]
+
+    if any(f.endswith(('.dll', '.txt', '.api', '.htm', '.html', '.py', '.js', '.ts', '.cmake', 'LICENSE', '.rst', '.ttf', '.xml', '.css', '.po', '.mo', '.pak', '.json', '.vdf', '.layout', '.pdf', '.mpp', '.jar', '.otf', '.md', '.pmp', '.png', '.gif', '.webp', '.jpg', '.maifest', '.TXT', '.tex', '.h', '.cpp', '.vbs', '.msg', '.lib', '.c', '.lua', '.mlua', '.wlua', '.dlua', '.mp4', '.mp3', '.so', '.dat', '.policy', '.properties', '.svg', '.conf', '.bin', '.frag', '.vert', '.bat', '.in', '.enc', '.tcl', '.sample', '.crt', '.pm', '.pl', '.code-snippets', '.env', '.rsh', '.bc', '.b', '.out', '.java')) for f in files):
+        continue
+    
+    for file in files:
+        s = file
+        print(len(s) * " ", '\r', s, end='')
+        time.sleep(0.1)
+        if file.startswith(f"{program_name}"):
+            found_file = (os.path.join(root, file))
+            console.print(f"\n\n[cyan1]After searching For [red]{program_name}.exe[/red], I found [red]{found_file}[/red]![/cyan1]\n\n")
+            time.sleep(1)
+            console.print("[cyan1]Alright!, I'm going to go ahead and get the wheels spinning...[/cyan1]\n\n")
+            time.sleep(1)
+            console.print("[cyan1]GETTING PROGRAMS MENU[/cyan1]")
+            objShell = win32com.client.Dispatch("WScript.Shell")
+            UserProgramsMenu = objShell.SpecialFolders("AllUsersPrograms")
+            time.sleep(1)
+            console.print("\n[cyan1]FETCH PROGRAMS MENU: COMPLETE[/cyan1]")
+            time.sleep(1)
+            console.print("\n[cyan1] Copying File...[/cyan1]")
+            shutil.copy(found_file, UserProgramsMenu)
+            console.print("\n[cyan1]File Has Been Copied, Restart Your Computer And Watch The Magic Happen.")
+            end = time.time()
+            elasped_time = end - start
+            console.print(f"\n[cyan1]This Process Took [red]{elasped_time}[/red] Seconds.")
+            time.sleep(2)
+            raise SystemExit
+
+       
+
